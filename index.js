6ae4a249ea5da81a33a1e32e434c4cf00ae9114c
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-05-13 16:08:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-/* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
 
-function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
-}
-
- 
 function greet(timeStr){
-    const hour = parseInt(timeStr, 10);
-    if (hour < 12) return "Good Morning"
-    if (hour > 17) return "Good Evening"
-    return "Good Afternoon"
+  let hour = parseInt(timeStr, 10)
+  if (hour < 12)
+  return "Good Morning"
+  if (hour > 12 && hour < 17)
+  return "Good Afternoon"
+  else
+  return "Good Evening"
 }
 
 function displayMessage(msg){
   document.getElementById("greeting").innerText = msg;
- }
+}
 
-
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
